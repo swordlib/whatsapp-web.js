615f6c04f6d9459b508a62ec97179d022d4551b4
fallenleavesguy <9261562+fallenleavesguy@users.noreply.github.com>
2026-07-29 17:04:34 +0800

    refactor: remove unused exports from web-sdk.js

--- src/util/Injected/web-sdk.js
+++ src/util/Injected/web-sdk.js
@@ -1,11 +1,5 @@
-const { ExposeAuthStore } = require('./AuthStore/AuthStore');
 const { LoadUtils } = require('./Utils');
-const { ExposeStore } = require('./Store');
-const Structures = require('./structures/index');
 
 module.exports = {
-    ExposeAuthStore,
-    ExposeStore,
     LoadUtils,
-    Structures,
 };
